6f882824fb0773a6768d8414cd686526ef4cb374
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-05-01 15:39:57 +0530

    Tracking changes made: Making commit to swap branch

--- cks/unit_tests/test_fdtd_lt.py
+++ cks/unit_tests/test_fdtd_lt.py
@@ -60,21 +60,13 @@ def test_mode1():
     Bx = af.data.constant(0, X.shape[0], X.shape[1], dtype=af.Dtype.f64)
     By = af.data.constant(0, X.shape[0], X.shape[1], dtype=af.Dtype.f64)
 
-    Bx[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x] =\
-    0.01 * af.cos(config[i].k_x*X[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x] +\
-                  config[i].k_y*Y[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x]) -\
-    0.02 * af.sin(config[i].k_x*X[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x] +\
-                  config[i].k_y*Y[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x])
-
-    By[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x] =\
-    0.02 * af.cos(config[i].k_x*X[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x] +\
-                  config[i].k_y*Y[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x]) -\
-    0.04 * af.sin(config[i].k_x*X[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x] +\
-                  config[i].k_y*Y[N_ghost_y:-N_ghost_y, N_ghost_x:-N_ghost_x])
+    B_x = 0.01 * af.cos(config[i].k_x*X + config[i].k_y*Y) - 0.02 * af.sin(config[i].k_x*X + config[i].k_y*Y)
+    B_y = 0.05 * af.cos(config[i].k_x*X + config[i].k_y*Y) - 0.01 * af.sin(config[i].k_x*X + config[i].k_y*Y)
+    E_z = af.constant(0, B_x.shape[0], B_x.shape[1], dtype = af.Dtype.f64)
 
 
     delta_B_x_hat = 0.01 + 0.02*1j
-    delta_B_y_hat = 0.02 + 0.04*1j
+    delta_B_y_hat = 0.05 + 0.01*1j
     delta_E_z_hat = 0
     delta_J_z_hat = 0
 
@@ -136,4 +128,75 @@ def test_mode1():
   #        abs(poly_E_z[0]+2)<0.3
   #       )
 
-test_mode1()
+def test_mode2():
+  config     = []
+  config_32  = initialize.set(N_32)
+  config.append(config_32)
+  config_64  = initialize.set(N_64)
+  config.append(config_64)
+  config_128 = initialize.set(N_128)
+  config.append(config_128)
+  config_256 = initialize.set(N_256)
+  config.append(config_256)
+  config_512 = initialize.set(N_512)
+  config.append(config_512)
+
+  error_E_x = np.zeros(len(config))
+  error_E_y = np.zeros(len(config))
+  error_B_z = np.zeros(len(config))
+
+  for i in range(len(config)):
+    X = initialize.calculate_x(config[i])[:, :, 0, 0]
+    Y = initialize.calculate_y(config[i])[:, :, 0, 0]
+
+    left_boundary  = config[i].left_boundary
+    right_boundary = config[i].right_boundary
+    bot_boundary   = config[i].bot_boundary
+    top_boundary   = config[i].top_boundary
+
+    length_x = right_boundary - left_boundary
+    length_y = top_boundary - bot_boundary
+
+    N_x = config[i].N_x
+    N_y = config[i].N_y
+
+    N_ghost_x = config[i].N_ghost_x
+    N_ghost_y = config[i].N_ghost_y
+
+    dx = (length_x/(N_x - 1))
+    dy = (length_y/(N_y - 1))
+
+    dt = (dx/4) + (dy/4)
+
+    final_time = 1
+    time       = np.arange(0, final_time, dt)
+
+    E_x = 0.01 * af.cos(config[i].k_x*X + config[i].k_y*Y) - 0.02 * af.sin(config[i].k_x*X + config[i].k_y*Y)
+    E_y = 0.05 * af.cos(config[i].k_x*X + config[i].k_y*Y) - 0.01 * af.sin(config[i].k_x*X + config[i].k_y*Y)
+    B_z = af.constant(0, E_x.shape[0], E_x.shape[1], dtype = af.Dtype.f64)
+
+    delta_E_x_hat = 0.01 + 0.02*1j
+    delta_E_y_hat = 0.05 + 0.01*1j
+    delta_B_z_hat = 0
+    delta_J_x_hat = 0
+    delta_J_y_hat = 0
+
+    J_x, J_y = 0, 0
+
+    data = np.zeros(time.size)
+    data_lt = np.zeros(time.size)
+
+    for time_index, t0 in enumerate(time):
+      B_z, E_x, E_y = fdtd.mode2_fdtd(config[i], B_z, E_x, E_y, J_x, J_y, dt)
+      delta_B_z_hat, delta_E_x_hat, delta_E_y_hat = EM_fields_mode2(delta_B_z_hat, delta_E_x_hat,\
+                                                                    delta_E_y_hat, delta_J_x_hat, 0,\
+                                                                    config[i].k_x, config[i].k_x, dt
+                                                                   )
+      data[time_index] = af.max(af.abs(B_z))
+      data_lt[time_index] = np.abs(delta_B_z_hat)
+
+    pl.plot(time, data_lt, '--', color = 'black')
+    pl.plot(time, data)
+    pl.show()
+
+test_mode2()
