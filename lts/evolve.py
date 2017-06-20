2f3111fe567c55d6b88ebbd404f822057dbf4c3b
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-06-20 18:29:24 +0530

    Implemented RK6

--- lts/evolve.py
+++ lts/evolve.py
@@ -61,7 +61,7 @@ def ddelta_f_hat_dt(config, delta_f_hat,\
          ddelta_B_x_hat_dt, ddelta_B_y_hat_dt, ddelta_B_z_hat_dt
         )
 
-def RK4_step(config, delta_f_hat, dt):
+def RK6_step(config, delta_f_hat, dt):
   
   global delta_E_x_hat, delta_E_y_hat, delta_E_z_hat
   global delta_B_x_hat, delta_B_y_hat, delta_B_z_hat
@@ -71,33 +71,64 @@ def RK4_step(config, delta_f_hat, dt):
                        delta_B_x_hat, delta_B_y_hat, delta_B_z_hat
                       )
   
-  k2 = ddelta_f_hat_dt(config, delta_f_hat + 0.5*k1[0]*dt,\
-                       delta_E_x_hat + 0.5*k1[1]*dt, delta_E_y_hat + 0.5*k1[2]*dt,\
-                       delta_E_z_hat + 0.5*k1[3]*dt, delta_B_x_hat + 0.5*k1[4]*dt,\
-                       delta_B_y_hat + 0.5*k1[5]*dt, delta_B_z_hat + 0.5*k1[6]*dt
+  k2 = ddelta_f_hat_dt(config, delta_f_hat + 0.25*k1[0]*dt,\
+                       delta_E_x_hat + 0.25*k1[1]*dt, delta_E_y_hat + 0.25*k1[2]*dt,\
+                       delta_E_z_hat + 0.25*k1[3]*dt, delta_B_x_hat + 0.25*k1[4]*dt,\
+                       delta_B_y_hat + 0.25*k1[5]*dt, delta_B_z_hat + 0.25*k1[6]*dt
                       )
   
-  k3 = ddelta_f_hat_dt(config, delta_f_hat + 0.5*k2[0]*dt,\
-                       delta_E_x_hat + 0.5*k2[1]*dt, delta_E_y_hat + 0.5*k2[2]*dt,\
-                       delta_E_z_hat + 0.5*k2[3]*dt, delta_B_x_hat + 0.5*k2[4]*dt,\
-                       delta_B_y_hat + 0.5*k2[5]*dt, delta_B_z_hat + 0.5*k2[6]*dt
+  k3 = ddelta_f_hat_dt(config, delta_f_hat + (3/32)*(k1[0]+3*k2[0])*dt,\
+                       delta_E_x_hat + (3/32)*(k1[1]+3*k2[1])*dt,\
+                       delta_E_y_hat + (3/32)*(k1[2]+3*k2[2])*dt,\
+                       delta_E_z_hat + (3/32)*(k1[3]+3*k2[3])*dt,\
+                       delta_B_x_hat + (3/32)*(k1[4]+3*k2[4])*dt,\
+                       delta_B_y_hat + (3/32)*(k1[5]+3*k2[5])*dt,\
+                       delta_B_z_hat + (3/32)*(k1[6]+3*k2[6])*dt
                       )
 
-  k4 = ddelta_f_hat_dt(config, delta_f_hat + 0.5*k3[0]*dt,\
-                       delta_E_x_hat + 0.5*k3[1]*dt, delta_E_y_hat + 0.5*k3[2]*dt,\
-                       delta_E_z_hat + 0.5*k3[3]*dt, delta_B_x_hat + 0.5*k3[4]*dt,\
-                       delta_B_y_hat + 0.5*k3[5]*dt, delta_B_z_hat + 0.5*k3[6]*dt
+  k4 = ddelta_f_hat_dt(config, delta_f_hat + (12/2197)*(161*k1[0]-600*k2[0]+608*k3[0])*dt,\
+                       delta_E_x_hat + (12/2197)*(161*k1[1]-600*k2[1]+608*k3[1])*dt,\
+                       delta_E_y_hat + (12/2197)*(161*k1[2]-600*k2[2]+608*k3[2])*dt,\
+                       delta_E_z_hat + (12/2197)*(161*k1[3]-600*k2[3]+608*k3[3])*dt,\
+                       delta_B_x_hat + (12/2197)*(161*k1[4]-600*k2[4]+608*k3[4])*dt,\
+                       delta_B_y_hat + (12/2197)*(161*k1[5]-600*k2[5]+608*k3[5])*dt,\
+                       delta_B_z_hat + (12/2197)*(161*k1[6]-600*k2[6]+608*k3[6])*dt
+                      )
+  
+  k5 = ddelta_f_hat_dt(config, delta_f_hat + (1/4104)*(8341*k1[0]-32832*k2[0]+29440*k3[0]-845*k4[0])*dt,\
+                       delta_E_x_hat + (1/4104)*(8341*k1[1]-32832*k2[1]+29440*k3[1]-845*k4[1])*dt,\
+                       delta_E_y_hat + (1/4104)*(8341*k1[2]-32832*k2[2]+29440*k3[2]-845*k4[2])*dt,\
+                       delta_E_z_hat + (1/4104)*(8341*k1[3]-32832*k2[3]+29440*k3[3]-845*k4[3])*dt,\
+                       delta_B_x_hat + (1/4104)*(8341*k1[4]-32832*k2[4]+29440*k3[4]-845*k4[4])*dt,\
+                       delta_B_y_hat + (1/4104)*(8341*k1[5]-32832*k2[5]+29440*k3[5]-845*k4[5])*dt,\
+                       delta_B_z_hat + (1/4104)*(8341*k1[6]-32832*k2[6]+29440*k3[6]-845*k4[6])*dt
+                      )
+ 
+  k6 = ddelta_f_hat_dt(config, delta_f_hat + \
+                       (-(8/27)*k1[0]+2*k2[0]-(3544/2565)*k3[0]+(1859/4104)*k4[0]-(11/40)*k5[0])*dt,\
+                       delta_E_x_hat + \
+                       (-(8/27)*k1[1]+2*k2[1]-(3544/2565)*k3[1]+(1859/4104)*k4[1]-(11/40)*k5[1])*dt,\
+                       delta_E_y_hat + \
+                       (-(8/27)*k1[2]+2*k2[2]-(3544/2565)*k3[2]+(1859/4104)*k4[2]-(11/40)*k5[2])*dt,\
+                       delta_E_z_hat + \
+                       (-(8/27)*k1[3]+2*k2[3]-(3544/2565)*k3[3]+(1859/4104)*k4[3]-(11/40)*k5[3])*dt,\
+                       delta_B_x_hat + \
+                       (-(8/27)*k1[4]+2*k2[4]-(3544/2565)*k3[4]+(1859/4104)*k4[4]-(11/40)*k5[4])*dt,\
+                       delta_B_y_hat + \
+                       (-(8/27)*k1[5]+2*k2[5]-(3544/2565)*k3[5]+(1859/4104)*k4[5]-(11/40)*k5[5])*dt,\
+                       delta_B_z_hat + \
+                       (-(8/27)*k1[6]+2*k2[6]-(3544/2565)*k3[6]+(1859/4104)*k4[6]-(11/40)*k5[6])*dt
                       )
 
-  delta_f_hat   += ((k1[0]+2*k2[0]+2*k3[0]+k4[0])/6)*dt
+  delta_f_hat   += 1/5*((16/27)*k1[0]+(6656/2565)*k3[0]+(28561/11286)*k4[0]-(9/10)*k5[0]+(2/11)*k6[0])*dt
 
-  delta_E_x_hat += ((k1[1]+2*k2[1]+2*k3[1]+k4[1])/6)*dt
-  delta_E_y_hat += ((k1[2]+2*k2[2]+2*k3[2]+k4[2])/6)*dt
-  delta_E_z_hat += ((k1[3]+2*k2[3]+2*k3[3]+k4[3])/6)*dt
+  delta_E_x_hat += 1/5*((16/27)*k1[1]+(6656/2565)*k3[1]+(28561/11286)*k4[1]-(9/10)*k5[1]+(2/11)*k6[1])*dt
+  delta_E_y_hat += 1/5*((16/27)*k1[2]+(6656/2565)*k3[2]+(28561/11286)*k4[2]-(9/10)*k5[2]+(2/11)*k6[2])*dt
+  delta_E_z_hat += 1/5*((16/27)*k1[3]+(6656/2565)*k3[3]+(28561/11286)*k4[3]-(9/10)*k5[3]+(2/11)*k6[3])*dt
 
-  delta_B_x_hat += ((k1[4]+2*k2[4]+2*k3[4]+k4[5])/6)*dt
-  delta_B_y_hat += ((k1[5]+2*k2[5]+2*k3[5]+k4[4])/6)*dt
-  delta_B_z_hat += ((k1[6]+2*k2[6]+2*k3[6]+k4[6])/6)*dt
+  delta_B_x_hat += 1/5*((16/27)*k1[4]+(6656/2565)*k3[4]+(28561/11286)*k4[4]-(9/10)*k5[4]+(2/11)*k6[4])*dt
+  delta_B_y_hat += 1/5*((16/27)*k1[5]+(6656/2565)*k3[5]+(28561/11286)*k4[5]-(9/10)*k5[5]+(2/11)*k6[5])*dt
+  delta_B_z_hat += 1/5*((16/27)*k1[6]+(6656/2565)*k3[6]+(28561/11286)*k4[6]-(9/10)*k5[6]+(2/11)*k6[6])*dt
 
   return(delta_f_hat)
 
@@ -142,7 +173,7 @@ def time_integration(config, delta_f_hat_initial, time_array):
     
     dt = time_array[1] - time_array[0]
 
-    delta_f_hat = RK4_step(config, delta_f_hat, dt)
+    delta_f_hat = RK6_step(config, delta_f_hat, dt)
 
     delta_rho_hat                = np.sum(delta_f_hat) * dv_x * dv_y * dv_z
     density_data[time_index + 1] = np.abs(delta_rho_hat)
