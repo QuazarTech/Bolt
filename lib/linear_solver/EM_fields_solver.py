c66e434f534c391de427ba7ed9ce6c41560a58e2
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-07-17 13:57:14 +0530

    Corrected electrostatic solver in linear solver/unit test added - A correction was made to the linear solver - A unit test ensuring correctness of the function was written

--- lib/linear_solver/EM_fields_solver.py
+++ lib/linear_solver/EM_fields_solver.py
@@ -2,19 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import arrayfire as af
-# import pylab as pl
 
 def compute_electrostatic_fields(self):
   """
   Computes the electrostatic fields by making use of the Poisson equation:
   div^2 phi = rho
   """
-  rho_hat = self.physical_system.params.charge_electron * \
-            2 * af.fft2(self.compute_moments('density'))/(self.N_q1 * self.N_q2)
+  rho_hat = 2 * af.fft2(self.compute_moments('density'))/(self.N_q1 * self.N_q2)
   rho_hat = af.tile(rho_hat, 1, 1, self.N_p1 * self.N_p2 * self.N_p3)
-  phi_hat = self.physical_system.params.charge_electron * rho_hat/(self.k_q1**2 + self.k_q2**2)
+  phi_hat = self.charge_electron * rho_hat/(self.k_q1**2 + self.k_q2**2)
   
-  phi_hat[0, 0] = 0
+  phi_hat[0, 0, :] = 0
 
   self.E1_hat = -phi_hat * (1j * self.k_q1)
   self.E2_hat = -phi_hat * (1j * self.k_q2)
@@ -23,10 +21,5 @@ def compute_electrostatic_fields(self):
   self.B1_hat = af.constant(0, self.N_q1, self.N_q2, self.N_p1*self.N_p2*self.N_p3, dtype = af.Dtype.c64)
   self.B2_hat = af.constant(0, self.N_q1, self.N_q2, self.N_p1*self.N_p2*self.N_p3, dtype = af.Dtype.c64) 
   self.B3_hat = af.constant(0, self.N_q1, self.N_q2, self.N_p1*self.N_p2*self.N_p3, dtype = af.Dtype.c64)
-
-  # E = 0.5 * self.N_q1 * self.N_q2 * af.ifft2(self.E1_hat)
-
-  # pl.plot(E[:, :, 0])
-  # pl.show()
 
   return
--- lib/linear_solver/tests/test_EM_fields_solver.py
+++ lib/linear_solver/tests/test_EM_fields_solver.py
@@ -1,13 +1,62 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
 
-# WORK IN PROGRESS 
-import arrayfire as af 
+import numpy as np
+import arrayfire as af
+from scipy.fftpack import fftfreq 
 
 from lib.linear_solver.EM_fields_solver import compute_electrostatic_fields
 
 class test(object):
+  def __init__(self):
+    self.charge_electron = -1
+    
+    self.N_q1 = 32
+    self.N_q2 = 34
+    
+    self.N_p1 = 2
+    self.N_p2 = 3
+    self.N_p3 = 4
+
+    self.k_q1 = 2 * np.pi * fftfreq(self.N_q1, 1/self.N_q1)
+    self.k_q2 = 2 * np.pi * fftfreq(self.N_q2, 1/self.N_q2)
+
+    self.k_q2, self.k_q1 = np.meshgrid(self.k_q2, self.k_q1)
+
+    self.k_q1 = af.tile(af.to_array(self.k_q1), 1, 1, self.N_p1 * self.N_p2 * self.N_p3)
+    self.k_q2 = af.tile(af.to_array(self.k_q2), 1, 1, self.N_p1 * self.N_p2 * self.N_p3)
+
+    self.q1 = af.to_array((0.5 + np.arange(self.N_q1)) * (1/self.N_q1))
+    self.q2 = af.to_array((0.5 + np.arange(self.N_q2)) * (1/self.N_q2))
+
+    self.q1 = af.tile(self.q1, 1, self.N_q2, self.N_p1 * self.N_p2 * self.N_p3)
+    self.q2 = af.tile(af.reorder(self.q2), self.N_q1, 1, self.N_p1 * self.N_p2 * self.N_p3)
+
+  def compute_moments(self, string):
+    return(1 + \
+           0.01 * af.cos(2*np.pi*self.q1[:, :, 0] + 4*np.pi*self.q2[:, :, 0]) - \
+           0.02 * af.sin(2*np.pi*self.q1[:, :, 0] + 4*np.pi*self.q2[:, :, 0])
+          )  
 
 def test_compute_electrostatic_fields():
 
+  test_obj = test()
   compute_electrostatic_fields(test_obj)
+
+  E1 = 0.5 * test_obj.N_q1 * test_obj.N_q2 * af.ifft2(test_obj.E1_hat)  
+  E2 = 0.5 * test_obj.N_q1 * test_obj.N_q2 * af.ifft2(test_obj.E2_hat)  
+
+  E1_analytical = test_obj.charge_electron * 2*np.pi/(20 * np.pi**2) *\
+                  (0.01 * af.sin(2*np.pi*test_obj.q1 + 4*np.pi*test_obj.q2) +\
+                   0.02 * af.cos(2*np.pi*test_obj.q1 + 4*np.pi*test_obj.q2)
+                  )
+
+  E2_analytical = test_obj.charge_electron * 4*np.pi/(20 * np.pi**2) *\
+                  (0.01 * af.sin(2*np.pi*test_obj.q1 + 4*np.pi*test_obj.q2) +\
+                   0.02 * af.cos(2*np.pi*test_obj.q1 + 4*np.pi*test_obj.q2)
+                  )
+
+  error_E1 = af.sum(af.abs(E1_analytical - E1))
+  error_E2 = af.sum(af.abs(E2_analytical - E2))
+  
+  assert(error_E1<5e-14 and error_E2<5e-14)
