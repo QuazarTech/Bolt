7374366554d98c8a0a821c313aaa242b5db773f1
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-09-28 00:39:20 +0530

    Pushing to savio for testing

--- example_problems/nonrelativistic_boltzmann/dispersion_relations/domain.py
+++ example_problems/nonrelativistic_boltzmann/dispersion_relations/domain.py
@@ -1,6 +1,6 @@
 q1_start = 0
 q1_end   = 1
-N_q1     = 16
+N_q1     = 32
 
 q2_start = 0
 q2_end   = 1
@@ -8,7 +8,7 @@ N_q2     = 3
 
 p1_start = -10
 p1_end   = 10
-N_p1     = 32
+N_p1     = 128
 
 p2_start = -0.5
 p2_end   = 0.5
--- example_problems/nonrelativistic_boltzmann/dispersion_relations/main.py
+++ example_problems/nonrelativistic_boltzmann/dispersion_relations/main.py
@@ -1,8 +1,9 @@
 import arrayfire as af
 import numpy as np
-import pylab as pl
 import petsc4py
 import sys
+import h5py
+
 petsc4py.init(sys.argv)
 
 from bolt.lib.physical_system import physical_system
@@ -20,36 +21,6 @@ import bolt.src.nonrelativistic_boltzmann.collision_operator \
 
 import bolt.src.nonrelativistic_boltzmann.moment_defs as moment_defs
 
-# Optimized plot parameters to make beautiful plots:
-pl.rcParams['figure.figsize']  = 12, 7.5
-pl.rcParams['figure.dpi']      = 300
-pl.rcParams['image.cmap']      = 'jet'
-pl.rcParams['lines.linewidth'] = 1.5
-pl.rcParams['font.family']     = 'serif'
-pl.rcParams['font.weight']     = 'bold'
-pl.rcParams['font.size']       = 20
-pl.rcParams['font.sans-serif'] = 'serif'
-pl.rcParams['text.usetex']     = True
-pl.rcParams['axes.linewidth']  = 1.5
-pl.rcParams['axes.titlesize']  = 'medium'
-pl.rcParams['axes.labelsize']  = 'medium'
-
-pl.rcParams['xtick.major.size'] = 8
-pl.rcParams['xtick.minor.size'] = 4
-pl.rcParams['xtick.major.pad']  = 8
-pl.rcParams['xtick.minor.pad']  = 8
-pl.rcParams['xtick.color']      = 'k'
-pl.rcParams['xtick.labelsize']  = 'medium'
-pl.rcParams['xtick.direction']  = 'in'
-
-pl.rcParams['ytick.major.size'] = 8
-pl.rcParams['ytick.minor.size'] = 4
-pl.rcParams['ytick.major.pad']  = 8
-pl.rcParams['ytick.minor.pad']  = 8
-pl.rcParams['ytick.color']      = 'k'
-pl.rcParams['ytick.labelsize']  = 'medium'
-pl.rcParams['ytick.direction']  = 'in'
-
 # Defining the physical system to be solved:
 system = physical_system(domain,
                          boundary_conditions,
@@ -64,8 +35,8 @@ system = physical_system(domain,
 ls  = linear_solver(system)
 
 # Time parameters:
-dt      = 0.002
-t_final = 1.0
+dt      = 0.001
+t_final = 10
 
 time_array = np.arange(0, t_final + dt, dt)
 
@@ -73,26 +44,19 @@ time_array = np.arange(0, t_final + dt, dt)
 rho_data = np.zeros_like(time_array)
 
 for time_index, t0 in enumerate(time_array):
-    # print('Computing For Time =', t0)
+    print('Computing For Time =', t0)
 
     n = ls.compute_moments('density')
     rho_data[time_index] = af.max(n)
     ls.RK2_timestep(dt)
 
-pl.plot(time_array, rho_data)
-pl.ylabel(r'MAX($\rho$)')
-pl.xlabel('Time')
-pl.savefig('rho.png')
-pl.clf()
-
-f_hat = abs(np.fft.fft(rho_data - 1))
+f_hat = abs(np.fft.fft(rho_data - np.min(rho_data)))
 omega = 2 * np.pi * np.fft.fftfreq(time_array.size, dt)
 
-pl.plot(omega, f_hat)
-pl.xlabel(r'$\omega$')
-pl.ylabel(r'$|FFT(\max(\rho(x))(t))|$')
-pl.xlim(-10, 10)
-pl.savefig('omega.png')
-pl.clf()
+h5f = h5py.File('data.h5', 'w')
+h5f.create_dataset('f_hat', data = f_hat)
+h5f.create_dataset('time', data = time_array)
+h5f.create_dataset('omega', data = omega)
+h5f.close()
 
-print('Omega:', omega[int(np.argmax(f_hat))])
+print('Omega:', omega[int(np.argmax(f_hat[:int(time_array.size/2)]))])
