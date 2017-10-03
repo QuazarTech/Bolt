dfe9a8bc4414b50829a8997b456065a166fa139e
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-10-03 13:48:07 +0530

    WIP test_apply_boundary_conditions

---
+++ bolt/lib/nonlinear_solver/tests/test_apply_boundary_conditions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+In this test, we ensure that the value returned by the function
+apply_boundary_conditions in nonlinear_solver is consistent with our expected
+results. Although both the values checked against and the generated values
+are essentially the same formulation, the failure of this test may indicate
+any accidental changes that may have been introduced.
+"""
