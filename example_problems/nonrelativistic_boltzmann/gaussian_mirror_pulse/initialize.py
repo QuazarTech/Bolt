df21f48ed71e0ebb54ff6e4d46a909ab5e762c8c
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-10-04 18:03:26 +0530

    Added gaussian mirror pulse files BUGGY ATM

---
+++ example_problems/nonrelativistic_boltzmann/gaussian_mirror_pulse/initialize.py
@@ -0,0 +1,26 @@
+"""
+Functions which are used in assigning the I.C's to
+the system.
+"""
+
+import arrayfire as af
+import numpy as np
+
+def initialize_f(q1, q2, p1, p2, p3, params):
+
+    m = params.mass_particle
+    k = params.boltzmann_constant
+
+    rho_b = params.rho_background
+
+    # Calculating the perturbed density:
+    rho = rho_b + 0.001 * af.exp(- 100*(q1 - 0.5)**2 
+                                 - 100*(q2 - 0.5)**2
+                                )
+
+    f = rho * (m / (2 * np.pi * k)) \
+            * af.exp(-m * (p1-1)**2 / (2 * k)) \
+            * af.exp(-m * p2**2 / (2 * k))
+
+    af.eval(f)
+    return (f)
---
+++ example_problems/nonrelativistic_boltzmann/gaussian_mirror_pulse/main.py
@@ -0,0 +1,88 @@
+import arrayfire as af
+import numpy as np
+import pylab as pl
+
+from bolt.lib.physical_system import physical_system
+
+from bolt.lib.nonlinear_solver.nonlinear_solver \
+    import nonlinear_solver
+
+import domain
+import boundary_conditions
+import params
+import initialize
+
+import bolt.src.nonrelativistic_boltzmann.advection_terms as advection_terms
+
+import bolt.src.nonrelativistic_boltzmann.collision_operator \
+    as collision_operator
+
+import bolt.src.nonrelativistic_boltzmann.moment_defs as moment_defs
+
+# Optimized plot parameters to make beautiful plots:
+pl.rcParams['figure.figsize']  = 12, 7.5
+pl.rcParams['figure.dpi']      = 300
+pl.rcParams['image.cmap']      = 'jet'
+pl.rcParams['lines.linewidth'] = 1.5
+pl.rcParams['font.family']     = 'serif'
+pl.rcParams['font.weight']     = 'bold'
+pl.rcParams['font.size']       = 20
+pl.rcParams['font.sans-serif'] = 'serif'
+pl.rcParams['text.usetex']     = True
+pl.rcParams['axes.linewidth']  = 1.5
+pl.rcParams['axes.titlesize']  = 'medium'
+pl.rcParams['axes.labelsize']  = 'medium'
+
+pl.rcParams['xtick.major.size'] = 8
+pl.rcParams['xtick.minor.size'] = 4
+pl.rcParams['xtick.major.pad']  = 8
+pl.rcParams['xtick.minor.pad']  = 8
+pl.rcParams['xtick.color']      = 'k'
+pl.rcParams['xtick.labelsize']  = 'medium'
+pl.rcParams['xtick.direction']  = 'in'
+
+pl.rcParams['ytick.major.size'] = 8
+pl.rcParams['ytick.minor.size'] = 4
+pl.rcParams['ytick.major.pad']  = 8
+pl.rcParams['ytick.minor.pad']  = 8
+pl.rcParams['ytick.color']      = 'k'
+pl.rcParams['ytick.labelsize']  = 'medium'
+pl.rcParams['ytick.direction']  = 'in'
+
+# Defining the physical system to be solved:
+system = physical_system(domain,
+                         boundary_conditions,
+                         params,
+                         initialize,
+                         advection_terms,
+                         collision_operator.BGK,
+                         moment_defs
+                        )
+
+# Declaring a linear system object which will evolve the defined physical system:
+nls = nonlinear_solver(system)
+
+# Time parameters:
+dt      = 0.001
+t_final = 0.5
+
+time_array = np.arange(0, t_final + dt, dt)
+
+def time_evolution():
+
+    for time_index, t0 in enumerate(time_array):
+        print('Computing For Time =', t0)
+
+        n_nls = nls.compute_moments('density')
+
+        af.display(af.flat(n_nls))
+
+        pl.contourf(np.array(nls.q1_center[:, :, 0]), np.array(nls.q2_center[:, :, 0]), 
+                    np.array(n_nls-1), 100, cmap = 'bwr')
+        pl.colorbar()
+        pl.savefig('%04d'%time_index + '.png')
+        pl.clf()
+
+        nls.strang_timestep(dt)
+        
+time_evolution()
