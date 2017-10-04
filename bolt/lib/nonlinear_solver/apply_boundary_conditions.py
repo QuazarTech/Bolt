6dde0f282ab7bd4a9accc2eadca413aa005d82dd
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-10-05 02:09:49 +0530

    Changed to compute moments and reverse direction of velocity for mirror BC. Push to test

--- bolt/lib/nonlinear_solver/apply_boundary_conditions.py
+++ bolt/lib/nonlinear_solver/apply_boundary_conditions.py
@@ -1,4 +1,10 @@
 import arrayfire as af
+import numpy as np
+
+@af.broadcast
+def f_MB(self, n, p1_b, T): 
+    return(n * (1 / (2 * np.pi * T))**0.5 * af.exp(-(self.p1-p1_b)**2 / (2 * T)))
+
 
 def apply_bcs_f(self):
     
@@ -60,20 +66,71 @@ def apply_bcs_f(self):
     elif(self.physical_system.boundary_conditions.in_q1 == 'mirror'):
 
         if(i_q1_start == 0):
-            self.f[:N_ghost] = af.flip(self.f[N_ghost:2 * N_ghost], 0)
-            self.f[:N_ghost] = \
-                self._convert_to_q_expanded(af.flip(self._convert_to_p_expanded(self.f), 
-                                                    1
-                                                   )
-                                           )[:N_ghost]
+            n_mirror  = self.compute_moments('density')
+            
+            p1_mirror = self.compute_moments('mom_p1_bulk') / n_mirror
+            p2_mirror = self.compute_moments('mom_p2_bulk') / n_mirror
+            p3_mirror = self.compute_moments('mom_p3_bulk') / n_mirror
+            
+            T_mirror  = (  self.compute_moments('energy')
+                         - n_mirror * p1_mirror**2
+                         - n_mirror * p2_mirror**2
+                         - n_mirror * p3_mirror**2
+                        ) / n_mirror
+
+            n_mirror[:N_ghost] = af.flip(n_mirror[N_ghost:2 * N_ghost], 0)
+            
+            p1_mirror[:N_ghost] = -af.flip(p1_mirror[N_ghost:2 * N_ghost], 0)
+            p2_mirror[:N_ghost] = af.flip(p2_mirror[N_ghost:2 * N_ghost], 0)
+            p3_mirror[:N_ghost] = af.flip(p3_mirror[N_ghost:2 * N_ghost], 0)
+            
+            T_mirror[:N_ghost] = af.flip(T_mirror[N_ghost:2 * N_ghost], 0)
+
+            f_mirror = f_MB(self, n_mirror, p1_mirror, T_mirror)
+
+            self.f[:N_ghost] = f_mirror[:N_ghost]
+
+            # self.f[:N_ghost] = af.flip(self.f[N_ghost:2 * N_ghost], 0)
+            # self.f[:N_ghost] = \
+            #     self._convert_to_q_expanded(af.flip(self._convert_to_p_expanded(self.f), 
+            #                                         1
+            #                                        )
+            #                                )[:N_ghost]
 
         if(i_q1_end == self.N_q1 - 1):
-            self.f[-N_ghost:] = af.flip(self.f[-2 * N_ghost:-N_ghost], 0)
-            self.f[-N_ghost:] = \
-                self._convert_to_q_expanded(af.flip(self._convert_to_p_expanded(self.f), 
-                                                    1
-                                                   )
-                                           )[-N_ghost:]
+            n_mirror  = self.compute_moments('density')
+            
+            p1_mirror = self.compute_moments('mom_p1_bulk') / n_mirror
+            p2_mirror = self.compute_moments('mom_p2_bulk') / n_mirror
+            p3_mirror = self.compute_moments('mom_p3_bulk') / n_mirror
+            
+            T_mirror  = (  self.compute_moments('energy')
+                         - n_mirror * p1_mirror**2
+                         - n_mirror * p2_mirror**2
+                         - n_mirror * p3_mirror**2
+                        ) / n_mirror
+
+            n_mirror[-N_ghost:] = af.flip(n_mirror[-2 * N_ghost:-N_ghost], 0)
+            
+            p1_mirror[-N_ghost:] = -af.flip(p1_mirror[-2 * N_ghost:-N_ghost], 0)
+            p2_mirror[-N_ghost:] =  af.flip(p2_mirror[-2 * N_ghost:-N_ghost], 0)
+            p3_mirror[-N_ghost:] =  af.flip(p3_mirror[-2 * N_ghost:-N_ghost], 0)
+            
+            T_mirror[-N_ghost:] = af.flip(T_mirror[-2 * N_ghost:-N_ghost], 0)
+            
+            f_mirror = f_MB(self, n_mirror, p1_mirror, T_mirror)
+
+            # f_mirror = n_mirror * (1 / (2 * np.pi * T_mirror))**0.5 \
+            #                     * af.exp(-(self.p1-p1_mirror)**2 / (2 * T_mirror))
+
+            self.f[-N_ghost:] = f_mirror[-N_ghost:]
+
+            # self.f[-N_ghost:] = af.flip(self.f[-2 * N_ghost:-N_ghost], 0)
+            # self.f[-N_ghost:] = \
+            #     self._convert_to_q_expanded(af.flip(self._convert_to_p_expanded(self.f), 
+            #                                         1
+            #                                        )
+            #                                )[-N_ghost:]
 
     elif(self.physical_system.boundary_conditions.in_q1 == 'shear'):
         # Not-implemented
