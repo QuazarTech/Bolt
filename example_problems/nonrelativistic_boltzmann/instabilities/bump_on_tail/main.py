c5456a90c240d7ece35c3a8bba026555aa31f782
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-11-02 19:05:40 +0530

    Changed bump on tail files

--- example_problems/nonrelativistic_boltzmann/instabilities/bump_on_tail/main.py
+++ example_problems/nonrelativistic_boltzmann/instabilities/bump_on_tail/main.py
@@ -53,54 +53,64 @@ pl.rcParams['ytick.labelsize']  = 'medium'
 pl.rcParams['ytick.direction']  = 'in'
 
 # Defining the physical system to be solved:
+linearized_system = physical_system(domain,
+                                    boundary_conditions,
+                                    params,
+                                    initialize,
+                                    advection_terms,
+                                    collision_operator.linearized_BGK,
+                                    moment_defs
+                                   )
+
 system = physical_system(domain,
                          boundary_conditions,
                          params,
                          initialize,
                          advection_terms,
-                         collision_operator.linearized_BGK,
+                         collision_operator.BGK,
                          moment_defs
                         )
 
 # Declaring a linear system object which will evolve the defined physical system:
-# nls = nonlinear_solver(system)
-ls  = linear_solver(system)
+nls = nonlinear_solver(system)
+ls  = linear_solver(linearized_system)
 
 # Time parameters:
-dt      = 0.001
-t_final = 20
+dt      = 0.0005
+t_final = 5
 
 time_array = np.arange(0, t_final + dt, dt)
 
 # Initializing Arrays used in storing the data:
 
 E_data_ls  = np.zeros_like(time_array)
-# E_data_nls = np.zeros_like(time_array)
+E_data_nls = np.zeros_like(time_array)
 
 def time_evolution():
 
     for time_index, t0 in enumerate(time_array):
-        # N_g                    = nls.N_ghost
-        # E_data_nls[time_index] = af.sum(nls.cell_centered_EM_fields[:, N_g:-N_g, N_g:-N_g]**2)
+        print(t0)
+        N_g                    = nls.N_ghost
+        E_data_nls[time_index] = af.sum(nls.cell_centered_EM_fields[:, N_g:-N_g, N_g:-N_g]**2)
         E1_ls                  = abs(ls.Y[1])
                                  # af.real(0.5 * (ls.N_q1 * ls.N_q2) 
                                  #             * af.ifft2(ls.E1_hat[:, :, 0])
                                  #        )
 
-        E_data_ls[time_index]  = E1_ls**2
+        E_data_ls[time_index]  = 192 * E1_ls**2
 
-        # nls.strang_timestep(dt)
-        ls.RK2_timestep(dt)
+        nls.strang_timestep(dt)
+        ls.RK6_timestep(dt)
         
 time_evolution()
 
 h5f = h5py.File('data.h5', 'w')
 h5f.create_dataset('electrical_energy_ls', data = E_data_ls)
-# h5f.create_dataset('electrical_energy_nls', data = E_data_nls)
+h5f.create_dataset('electrical_energy_nls', data = E_data_nls)
 h5f.close()
 
 pl.plot(time_array, E_data_ls, '--', color = 'black', label = 'Linear Solver')
-# pl.plot(time_array, E_data_nls, label='Nonlinear Solver')
+pl.plot(time_array, E_data_nls, label='Nonlinear Solver')
 pl.ylabel(r'SUM($|E|^2$)')
 pl.xlabel('Time')
 pl.legend()
@@ -108,7 +118,7 @@ pl.savefig('linearplot.png')
 pl.clf()
 
 pl.semilogy(time_array, E_data_ls, '--', color = 'black', label = 'Linear Solver')
-# pl.semilogy(time_array, E_data_nls, label='Nonlinear Solver')
+pl.semilogy(time_array, E_data_nls, label='Nonlinear Solver')
 pl.ylabel(r'SUM($|E|^2$)')
 pl.xlabel('Time')
 pl.legend()
