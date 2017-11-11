5fa4daddcfd5d259c0831533c832ebdb3b62c20d
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-11-11 12:02:40 +0530

    Modified Sod shock setup

--- example_problems/nonrelativistic_boltzmann/shock_tests/sod_shock_test/initialize.py
+++ example_problems/nonrelativistic_boltzmann/shock_tests/sod_shock_test/initialize.py
@@ -13,8 +13,8 @@ def initialize_f(q1, q2, p1, p2, p3, params):
     m = params.mass_particle
     k = params.boltzmann_constant
 
-    rho = af.select(q1<0.5, 1, 0.125)
-    T   = af.select(q1<0.5, 1, 0.8)
+    rho = af.select(q1<0.5, q1**0, 0.125)
+    T   = af.select(q1<0.5, q1**0, 0.8)
 
     f = rho * np.sqrt(m / (2 * np.pi * k * T)) \
             * af.exp(-m * p1**2 / (2 * k * T))
