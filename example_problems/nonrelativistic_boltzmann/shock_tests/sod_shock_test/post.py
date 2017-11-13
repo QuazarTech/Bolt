a9de6e9f4956b622f23036c62fac6f5552e789f2
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-11-14 01:11:39 +0530

    Changed the sod shock test for running on multiple nodes

--- example_problems/nonrelativistic_boltzmann/shock_tests/sod_shock_test/post.py
+++ example_problems/nonrelativistic_boltzmann/shock_tests/sod_shock_test/post.py
@@ -2,6 +2,7 @@ import numpy as np
 import pylab as pl
 import h5py
 import domain
+import params
 
 # Optimized plot parameters to make beautiful plots:
 pl.rcParams['figure.figsize']  = 10, 14
@@ -33,7 +34,7 @@ pl.rcParams['ytick.color']      = 'k'
 pl.rcParams['ytick.labelsize']  = 'medium'
 pl.rcParams['ytick.direction']  = 'in'
 
-dt      = 0.0001
+dt      = 0.00005
 t_final = 0.2
 time    = np.arange(dt, t_final + dt, dt)
 
@@ -41,27 +42,35 @@ N_q1 = domain.N_q1
 N_q2 = domain.N_q2
 N_g  = domain.N_ghost
 
-h5f  = h5py.File('dump/0000.h5', 'r')
-q1   = h5f['q1'][:].reshape(N_q1 + 2 * N_g, N_q2 + 2 * N_g)
-q2   = h5f['q2'][:].reshape(N_q1 + 2 * N_g, N_q2 + 2 * N_g)
-n    = h5f['n'][:].reshape(N_q1 + 2 * N_g, N_q2 + 2 * N_g)
-p1   = h5f['p1'][:].reshape(N_q1 + 2 * N_g, N_q2 + 2 * N_g)
-T    = h5f['T'][:].reshape(N_q1 + 2 * N_g, N_q2 + 2 * N_g)
+q1 = (0.5 + np.arange(N_q1)) * 1/N_q1
+
+h5f     = h5py.File('dump/0000.h5', 'r')
+moments = np.swapaxes(h5f['moments'][:], 0, 1)
 h5f.close()
+
+n       = moments[:, :, 0]
+p1_bulk = moments[:, :, 1] / n
+p2_bulk = moments[:, :, 2] / n
+p3_bulk = moments[:, :, 3] / n
+T       = (  moments[:, :, 4]
+           - n * p1_bulk**2
+           - n * p2_bulk**2
+           - n * p3_bulk**2
+          ) / (params.p_dim * n)
 
 fig = pl.figure()
 
 ax1 = fig.add_subplot(3,1,1)
-ax1.plot(q1[N_g:-N_g, N_g], n[N_g:-N_g, N_g])
+ax1.plot(q1, n[N_g:-N_g, N_g])
 ax1.set_ylabel(r'$\rho$')
 
 ax2 = fig.add_subplot(3,1,2)
-ax2.plot(q1[N_g:-N_g, N_g], p1[N_g:-N_g, N_g])
+ax2.plot(q1, p1_bulk[N_g:-N_g, N_g])
 ax2.set_ylabel(r'$v_x$')
 ax2.set_ylim([0, 1])
 
 ax3 = fig.add_subplot(3,1,3)
-ax3.plot(q1[N_g:-N_g, N_g], n[N_g:-N_g, N_g] * T[N_g:-N_g, N_g])
+ax3.plot(q1, n[N_g:-N_g, N_g] * T[N_g:-N_g, N_g])
 ax3.set_ylabel(r'$p$')
 ax3.set_xlabel('$x$')
 
@@ -71,26 +80,34 @@ pl.close(fig)
 pl.clf()
 
 for time_index, t0 in enumerate(time):
-    
+
     h5f  = h5py.File('dump/%04d'%(time_index+1) + '.h5', 'r')
-    n    = h5f['n'][:].reshape(N_q1 + 2 * N_g, N_q2 + 2 * N_g)
-    p1   = h5f['p1'][:].reshape(N_q1 + 2 * N_g, N_q2 + 2 * N_g)
-    T    = h5f['T'][:].reshape(N_q1 + 2 * N_g, N_q2 + 2 * N_g)
+    moments = np.swapaxes(h5f['moments'][:], 0, 1)
     h5f.close()
+    
+    n       = moments[:, :, 0]
+    p1_bulk = moments[:, :, 1] / n
+    p2_bulk = moments[:, :, 2] / n
+    p3_bulk = moments[:, :, 3] / n
+    T       = (  moments[:, :, 4]
+               - n * p1_bulk**2
+               - n * p2_bulk**2
+               - n * p3_bulk**2
+              ) / (params.p_dim * n)
 
     fig = pl.figure()
 
     ax1 = fig.add_subplot(3,1,1)
-    ax1.plot(q1[N_g:-N_g, N_g], n[N_g:-N_g, N_g])
+    ax1.plot(q1, n[N_g:-N_g, N_g])
     ax1.set_ylabel(r'$\rho$')
 
     ax2 = fig.add_subplot(3,1,2)
-    ax2.plot(q1[N_g:-N_g, N_g], p1[N_g:-N_g, N_g])
+    ax2.plot(q1, p1[N_g:-N_g, N_g])
     ax2.set_ylabel(r'$v_x$')
     ax2.set_ylim([0, 1])
 
     ax3 = fig.add_subplot(3,1,3)
-    ax3.plot(q1[N_g:-N_g, N_g], n[N_g:-N_g, N_g] * T[N_g:-N_g, N_g])
+    ax3.plot(q1, n[N_g:-N_g, N_g] * T[N_g:-N_g, N_g])
     ax3.set_ylabel(r'$p$')
     ax3.set_xlabel('$x$')
 
