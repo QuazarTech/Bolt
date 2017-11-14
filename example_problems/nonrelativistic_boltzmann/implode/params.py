fb43d33265cf78ceacc7904d0c26a786fca6f873
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-11-14 16:52:11 +0530

    Added broadcast function for tau in implode tesst

--- example_problems/nonrelativistic_boltzmann/implode/params.py
+++ example_problems/nonrelativistic_boltzmann/implode/params.py
@@ -31,5 +31,6 @@ boltzmann_constant = 1
 charge_electron    = 0
 
 # Variation of collisional-timescale parameter through phase space:
+@af.broadcast
 def tau(q1, q2, p1, p2, p3):
     return (0 * q1**0 * p1**0)
