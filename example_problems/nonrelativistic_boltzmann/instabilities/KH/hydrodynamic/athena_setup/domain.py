82e8718056bb995fb5d3ab31fd94097a0e1d2d6f
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-11-18 13:27:00 +0530

    Reverting back

--- example_problems/nonrelativistic_boltzmann/instabilities/KH/hydrodynamic/athena_setup/domain.py
+++ example_problems/nonrelativistic_boltzmann/instabilities/KH/hydrodynamic/athena_setup/domain.py
@@ -1,21 +1,21 @@
 q1_start = -0.5
 q1_end   = 0.5
-N_q1     = 32
+N_q1     = 256
 
 q2_start = -0.5
 q2_end   = 0.5
-N_q2     = 32
+N_q2     = 256
 
 p1_start = -10
 p1_end   = 10
-N_p1     = 16
+N_p1     = 24
 
 p2_start = -10
 p2_end   = 10
-N_p2     = 16
+N_p2     = 24
 
 p3_start = -10
 p3_end   = 10
-N_p3     = 16
+N_p3     = 24
 
 N_ghost = 3
