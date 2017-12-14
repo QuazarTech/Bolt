6a40658dddb71fb83d97c9149e33b220767fd37c
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-12-14 11:45:53 +0530

    Using a 2D2V setup to debug

---
+++ example_problems/nonrelativistic_boltzmann/shock_tests/brian_wu_shock_test/domain.py
@@ -0,0 +1,21 @@
+q1_start = 0
+q1_end   = 1
+N_q1     = 128
+
+q2_start = 0
+q2_end   = 1
+N_q2     = 3
+
+p1_start = -8
+p1_end   = 8
+N_p1     = 20
+
+p2_start = -8
+p2_end   = 8
+N_p2     = 20
+
+p3_start = -8
+p3_end   = 8
+N_p3     = 20
+
+N_ghost = 3
---
+++ example_problems/nonrelativistic_boltzmann/shock_tests/brian_wu_shock_test/initialize.py
@@ -0,0 +1,23 @@
+"""
+Functions which are used in assigning the I.C's to
+the system.
+"""
+
+import arrayfire as af
+import numpy as np
+
+def initialize_f(q1, q2, p1, p2, p3, params):
+
+    m     = params.mass_particle
+    k     = params.boltzmann_constant
+
+    rho = af.select(q1<0.5, q1**0, 0.125)
+    T   = af.select(q1<0.5, q1**0, 0.8)
+
+    f = rho * af.sqrt(m / (2 * np.pi * k * T))**3 \
+            * af.exp(-m * p1**2 / (2 * k * T)) \
+            * af.exp(-m * p2**2 / (2 * k * T)) \
+            * af.exp(-m * p3**2 / (2 * k * T))
+
+    af.eval(f)
+    return (f)
