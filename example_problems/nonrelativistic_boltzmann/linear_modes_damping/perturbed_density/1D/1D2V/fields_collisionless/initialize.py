1152d9e0d1d9451f2f0c2f5e85607a9335398782
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-12-14 13:52:31 +0530

    Made changes to the 1D2V setup

--- example_problems/nonrelativistic_boltzmann/linear_modes_damping/perturbed_density/1D/1D2V/fields_collisionless/initialize.py
+++ example_problems/nonrelativistic_boltzmann/linear_modes_damping/perturbed_density/1D/1D2V/fields_collisionless/initialize.py
@@ -27,9 +27,9 @@ def initialize_B(q1, q2, params):
     k_q1 = params.k_q1
     k_q2 = params.k_q2
 
-    B1 = 1 * q1**0
-    B2 = 2 * q1**0
-    B3 = af.cos(2 * np.pi * q1)
+    B1 = 0.01 * af.sin(2 * np.pi * q1) 
+    B2 = -0.01 * af.sin(2 * np.pi * q1)
+    B3 = 0.001 * af.cos(2 * np.pi * q1)
 
     return(B1, B2, B3)
 
