(1) Fixed bug in parallel SNES solver due to wrong shape of q1_2D, q2_2D (2) Added tau=0 option for defect collision operator (3) Added option to stagger Poisson solver params.electrostatic_solver_step

bolt/lib/nonlinear_solver/EM_fields_solver/electrostatic.py
@@ -118,8 +118,8 @@ class poisson_eqn_3D(object):
         q2_3D_data_structure, q3_3D_data_structure, q1_3D_data_structure = \
                 np.meshgrid(q2_3D, q3_3D, q1_3D)
         
-        q2_2D_data_structure, q1_2D_data_structure = \
-                np.meshgrid(q2_2D, q1_2D)
+        q1_2D_data_structure, q2_2D_data_structure = \
+                np.meshgrid(q1_2D, q2_2D)
 
         tol = 1e-10
         self.cond_3D =   (q1_3D_data_structure >= q1_2D[N_g] - tol) \
@@ -376,7 +376,7 @@ def compute_electrostatic_fields(self):
         af.sync()
         toc = af.time()
         self.time_fieldsolver += toc - tic
-    
+
     return
 
 

bolt/src/electronic_boltzmann/collision_operator.py
@@ -335,7 +335,15 @@ def f0_ee(f, p1, p2, p3, params):
 
 def RTA(f, q1, q2, p1, p2, p3, moments, params, flag = False):
     """Return BGK operator -(f-f0)/tau."""
+
+    if(af.any_true(params.tau_defect(q1, q2, p1, p2, p3) == 0)):
+        if (flag == False):
+            return(0.*f)
+
+        f = f0_defect_constant_T(f, p1, p2, p3, params)
     
+        return(f)
+
     C_f = -(  f - f0_defect_constant_T(f, p1, p2, p3, params)
            ) / params.tau_defect(q1, q2, p1, p2, p3) \
 #          -(  f - f0_ee(f, p1, p2, p3, params)
@@ -344,6 +352,7 @@ def RTA(f, q1, q2, p1, p2, p3, moments, params, flag = False):
     # WORKAROUND:
     C_f = af.select(params.tau_defect(q1, q2, p1, p2, p3) == np.inf, 0, C_f)
 #    C_f = af.select(params.tau_ee(q1, q2, p1, p2, p3)     == np.inf, 0, C_f)
-    
+
     af.eval(C_f)
     return(C_f)
+

example_problems/electronic_boltzmann/graphene/main.py
@@ -76,6 +76,7 @@ dt = 1e-4
 t_final = 1000.
 
 time_array = np.arange(dt, t_final + dt, dt)
+nls.f     = af.select(nls.f < 1e-13, 1e-13, nls.f)
 compute_electrostatic_fields(nls)
 
 N_g        = domain.N_ghost
@@ -84,20 +85,18 @@ print("rank = ", nls._comm.rank, " params.rank = ", params.rank,
       "mu = ", af.mean(params.mu[0, N_g:-N_g, N_g:-N_g]),
       "phi = ", af.mean(params.phi[N_g:-N_g, N_g:-N_g]),
       "density = ", af.mean(n_nls[0, N_g:-N_g, N_g:-N_g]),
-      "|E1| = ", af.mean(af.abs(nls.cell_centered_EM_fields[0])),
-      "|E2| = ", af.mean(af.abs(nls.cell_centered_EM_fields[1]))
+      "|E1| = ", af.mean(af.abs(nls.cell_centered_EM_fields[0, N_g:-N_g, N_g:-N_g])),
+      "|E2| = ", af.mean(af.abs(nls.cell_centered_EM_fields[1, N_g:-N_g, N_g:-N_g]))
      )
 
 t0       = 0.0
-time_step = 0
+params.time_step = time_step = 0
 while t0 < t_final:
 
     dump_steps = 1000
     if (time_step%dump_steps==0):
         nls.dump_moments('dumps/density_' + '%06d'%(time_step/dump_steps))
         nls.dump_distribution_function('dumps/f_' + '%06d'%(time_step/dump_steps))
-
-    time_step = time_step + 1
 
     dt_force_constraint = \
         0.5 * np.min(nls.dp1, nls.dp2) \
@@ -109,8 +108,6 @@ while t0 < t_final:
     dt_collision_constraint = 1e-3
 
     #dt = np.min(dt_force_constraint, dt_collision_constraint)
-
-    t0 = t0 + dt
 
     PETSc.Sys.Print("Time step =", time_step, ", Time =", t0, " dt = ",
                     dt_force_constraint
@@ -178,18 +175,21 @@ while t0 < t_final:
 #        pl.clf()
 
     nls.strang_timestep(dt)
+    time_step = time_step + 1
+    params.time_step = time_step
+    t0 = t0 + dt
 
     # Floors
     nls.f     = af.select(nls.f < 1e-13, 1e-13, nls.f)
-    params.mu = af.select(params.mu < 1e-13, 1e-13, params.mu)
+    #params.mu = af.select(params.mu < 1e-25, 1e-25, params.mu)
 
     n_nls = nls.compute_moments('density')
     print("rank = ", nls._comm.rank, " params.rank = ", params.rank,
           "mu = ", af.mean(params.mu[0, N_g:-N_g, N_g:-N_g]),
           "phi = ", af.mean(params.phi[N_g:-N_g, N_g:-N_g]),
           "density = ", mean_density,
-          "|E1| = ", af.mean(af.abs(nls.cell_centered_EM_fields[0])),
-          "|E2| = ", af.mean(af.abs(nls.cell_centered_EM_fields[1]))
+          "|E1| = ", af.mean(af.abs(nls.cell_centered_EM_fields[0, N_g:-N_g, N_g:-N_g])),
+          "|E2| = ", af.mean(af.abs(nls.cell_centered_EM_fields[1, N_g:-N_g, N_g:-N_g]))
          )
     PETSc.Sys.Print("--------------------\n")
 