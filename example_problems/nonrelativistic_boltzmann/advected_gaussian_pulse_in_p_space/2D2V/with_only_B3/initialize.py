699486e6da716c6f07d82e77f990922eb4fa3c4d
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2017-12-26 15:57:03 +0530

    Convergence test when only B3 is used

---
+++ example_problems/nonrelativistic_boltzmann/advected_gaussian_pulse_in_p_space/2D2V/with_only_B3/initialize.py
@@ -0,0 +1,26 @@
+"""
+Functions which are used in assigning the I.C's to
+the system.
+"""
+
+import arrayfire as af
+import numpy as np
+
+def initialize_f(q1, q2, p1, p2, p3, params):
+
+    m = params.mass_particle
+    k = params.boltzmann_constant
+
+    rho_b = params.rho_background
+    T_b   = params.temperature_background
+
+    p1_bulk = params.p1_bulk_background
+    p2_bulk = params.p2_bulk_background
+
+    rho = rho_b + 0 * q1
+    f   = rho * (m / (2 * np.pi * k * T_b)) \
+              * af.exp(-m * (p1 - p1_bulk)**2 / (2 * k * T_b)) \
+              * af.exp(-m * (p2 - p2_bulk)**2 / (2 * k * T_b)) \
+
+    af.eval(f)
+    return (f)
