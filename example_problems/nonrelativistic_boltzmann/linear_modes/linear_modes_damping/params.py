b4ab4edc439e99d32869e3599a92062dba4b2c67
Mani Chandra <manic@illinois.edu>
2018-01-22 23:12:52 +0530

    Convergence test notebook in linear_modes_damping/

--- example_problems/nonrelativistic_boltzmann/linear_modes/linear_modes_damping/params.py
+++ example_problems/nonrelativistic_boltzmann/linear_modes/linear_modes_damping/params.py
@@ -3,7 +3,7 @@ import arrayfire as af
 
 fields_type       = 'electrostatic'
 fields_initialize = 'fft'
-fields_solver     = 'fdtd'
+fields_solver     = 'fft'
 
 solver_method_in_q = 'FVM'
 solver_method_in_p = 'FVM'
@@ -13,6 +13,8 @@ reconstruction_method_in_p = 'weno5'
 
 riemann_solver_in_q = 'upwind-flux'
 riemann_solver_in_p = 'upwind-flux'
+
+instantaneous_collisions = 0
 
 # Dimensionality considered in velocity space:
 p_dim = 1
@@ -46,4 +48,4 @@ instantaneous_collisions = True
 # Variation of collisional-timescale parameter through phase space:
 @af.broadcast
 def tau(q1, q2, p1, p2, p3):
-    return (0 * p1**0 * q1**0)
+    return (np.inf * p1**0 * q1**0)
