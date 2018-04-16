1e38ff30c538ecdf6329938e1f43f2ca2a767554
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2018-04-16 13:56:27 +0530

    Corrected the grid staggerring for currents

--- bolt/lib/nonlinear/finite_volume/df_dt_fvm.py
+++ bolt/lib/nonlinear/finite_volume/df_dt_fvm.py
@@ -133,7 +133,7 @@ def df_dt_fvm(f, self):
                 J2 = curlB_2 / mu # (i + 1/2, j)
                 J3 = curlB_3 / mu # (i + 1/2, j + 1/2)
                 
-                # Applying Generalized Ohm's Law:
+                # Using Generalized Ohm's Law for electric field:
                 # (v X B)_x = B3 * v2 - B2 * v3
                 # (v X B)_x --> (i, j + 1/2)
                 v_cross_B_1 =   0.5 * (B3_plus_q2 + B3) * self.compute_moments('mom_v2_bulk', f = f_left) \
@@ -156,16 +156,16 @@ def df_dt_fvm(f, self):
 
                 # (J X B)_x = B3 * J2 - B2 * J3
                 # (J X B)_x --> (i, j + 1/2)
-                J_cross_B_1 =   0.5 * (B3_plus_q2 + B3) * (  J2 + af.shift(J2, 0, 0, 0, 1)
-                                                           + af.shift(J2, 0, 0, -1) + af.shift(J2, 0, 0, -1, 1)
+                J_cross_B_1 =   0.5 * (B3_plus_q2 + B3) * (  J2 + af.shift(J2, 0, 0, 0, -1)
+                                                           + af.shift(J2, 0, 0, 1) + af.shift(J2, 0, 0, 1, -1)
                                                           ) * 0.25 \
                               - B2                      * (af.shift(J3, 0, 0, 1) + J3) * 0.5
 
                 # (J X B)_y = B1 * J3 - B3 * J1
                 # (J X B)_y --> (i + 1/2, j)
                 J_cross_B_2 =   B1                      * (af.shift(J3, 0, 0, 0, 1) + J3) * 0.5 \
-                              - 0.5 * (B3_plus_q1 + B3) * (  J1 + af.shift(J1, 0, 0, 1, 0)
-                                                           + af.shift(J1, 0, 0, 0, -1) + af.shift(J1, 0, 0, 1, -1)
+                              - 0.5 * (B3_plus_q1 + B3) * (  J1 + af.shift(J1, 0, 0, 0, 1)
+                                                           + af.shift(J1, 0, 0, -1) + af.shift(J1, 0, 0, -1, 1)
                                                           ) * 0.25
 
                 # (J X B)_z = B2 * J1 - B1 * J2
