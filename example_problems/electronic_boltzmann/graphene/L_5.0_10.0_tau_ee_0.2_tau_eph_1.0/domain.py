8b179387b86e88f9e3e36f4e939c960bcbbe6380
gitansh95 <gitansh95@gmail.com>
2019-06-01 06:21:10 -0400

    Comments added to domain.py

--- example_problems/electronic_boltzmann/graphene/L_5.0_10.0_tau_ee_0.2_tau_eph_1.0/domain.py
+++ example_problems/electronic_boltzmann/graphene/L_5.0_10.0_tau_ee_0.2_tau_eph_1.0/domain.py
@@ -6,13 +6,22 @@ q2_start = 0.
 q2_end   = 10.
 N_q2     = 20
 
+# If N_p1 > 1, mirror boundary conditions require p1 to be
+# symmetric about zero
+
 p1_start = [-0.04]
 p1_end   =  [0.04]
 N_p1     =  16
 
+# If N_p2 > 1, mirror boundary conditions require p2 to be
+# symmetric about zero
+
 p2_start = [-0.04]
 p2_end   =  [0.04]
 N_p2     =  16
+
+# If N_p3 > 1, mirror boundary conditions require p3 to be
+# symmetric about zero
 
 p3_start = [-0.5]
 p3_end   =  [0.5]
